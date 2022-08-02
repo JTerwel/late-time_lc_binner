8b8b3bc77657ede16eb75014829739528cb1b396
JTerwel <82818204+JTerwel@users.noreply.github.com>
2022-08-02 15:18:44 +0100

    Another typo fixed

--- Binning_program.py
+++ Binning_program.py
@@ -162,7 +162,6 @@ class ztf_object:
 			self.lc = pd.read_csv(self.loc, usecols = self.cols, comment='#')
 		except: #In case something goes wrong when reading in
 			self.text += f'Could not read in lc for {self.name}\n'+ traceback.format_exc() + '\n'
-			print(self.name)
 			self.peak_mjd = 0
 			self.lc = pd.DataFrame(columns=self.cols)
 			return
